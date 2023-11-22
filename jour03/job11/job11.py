e9fa870720b20ce1f043cd53db6295dd1027262d
jean <jean@gmail.com>
2023-11-22 16:56:57 +0100

    job11

--- jour03/job11/job11.py
+++ jour03/job11/job11.py
@@ -1,14 +1,14 @@
 def time_to_text(minutes):
-    heures = minutes // 60
-    minutes_restantes = minutes % 60
-    if heures == 1:
-        heures_str = "1 heure"
+    hours = minutes // 60
+    minutes_remaining = minutes % 60
+    if hours == 1:
+        hours_str = "1 heure"
     else:
-        heures_str = f"{heures} heures"
+        hours_str = f"{hours} heures"
 
-    if minutes_restantes == 1:
+    if minutes_remaining == 1:
         minutes_str = "1 minute"
     else:
-        minutes_str = f"{minutes_restantes} minutes"
-    print(f"{heures_str} et {minutes_str}")
+        minutes_str = f"{minutes_remaining} minutes"
+    print(f"{hours_str} et {minutes_str}")
 time_to_text(190)
