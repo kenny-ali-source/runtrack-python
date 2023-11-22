0a45bcced283671f2ecd9b530e2221041baf4e4b
jean <jean@gmail.com>
2023-11-22 16:46:27 +0100

    ajout job10

---
+++ jour03/job10/job10.py
@@ -0,0 +1,13 @@
+def verifie_pair_impair(nombre):
+    if isinstance(nombre, int) and nombre >= 0:
+        if nombre % 2 == 0:
+            return "Le nombre est pair."
+        else:
+            return "Le nombre est impair."
+    else:
+        return "ce nombre n'est pas un entier positif."
+print(verifie_pair_impair(10))
+print(verifie_pair_impair(7))
+print(verifie_pair_impair(0))
+print(verifie_pair_impair(-5))
+print(verifie_pair_impair(3.14))
