782c2e9fa88734d0212bb50f457d8449a3b77ab0
jean <jean@gmail.com>
2023-11-27 14:30:41 +0100

    ajout job07

--- Jour05/job07/job07.py
+++ Jour05/job07/job07.py
@@ -1,15 +1,15 @@
 def function(grades):
-    notes_arrondies = []
+    round = []
     for i in grades:
         if i < 40:
-            notes_arrondies.append(i)
+            round.append(i)
         else:
-            prochain_multiple_de_5 = ((i // 5) + 1) * 5
-            if prochain_multiple_de_5 - i < 3:
-                notes_arrondies.append(prochain_multiple_de_5)
+            multiple_5 = ((i // 5) + 1) * 5
+            if multiple_5 - i < 3:
+               round.append(multiple_5)
             else:
-                notes_arrondies.append(i) 
-    return notes_arrondies
+                round.append(i) 
+    return round
 grades_list = [37, 82, 56, 91., 94.5]
-notes_arrondies = function(grades_list)
-print(notes_arrondies)
+round= function(grades_list)
+print(round)
